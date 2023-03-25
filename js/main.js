e5d3d3c8a0d13f4e434e10fde952b80b9d3481e5
probro27 <prabhavkhera@hotmail.com>
2023-03-25 16:52:44 -0400

    added dropdown to access graph of e-coli from backend

--- js/main.js
+++ js/main.js
@@ -1,28 +1,26 @@
+function onclick(_event) {
+    axios.post("http://127.0.0.1:5000/automata").then((response) => {
+        console.log(response)
+        let result = response.data.transitions;
+        let states = response.data.states;
+        drawGraph(result);
+    })
+}
 let g = new Graph();
-console.log(g);
-// g.Edge.style.directed = true;
 
-let result = [[[0, 0], [1, 1], [1, 1]], [[0, 0], [1, 0], [1, 2]], [[0, 0], [0, 1], [2, 1]], [[0, 0], [0, 0], [2, 2]], [[0, 1], [1, 1], [1, 2]], [[0, 1], [1, 0], [1, 0]], [[0, 1], [0, 1], [2, 2]], [[0, 1], [0, 0], [2, 0]], [[0, 2], [1, 1], [1, 0]], [[0, 2], [1, 0], [1, 1]], [[0, 2], [0, 1], [2, 0]], [[0, 2], [0, 0], [2, 1]], [[1, 0], [1, 1], [2, 1]], [[1, 0], [1, 0], [2, 2]], [[1, 0], [0, 1], [0, 1]], [[1, 0], [0, 0], [0, 2]], [[1, 1], [1, 1], [2, 2]], [[1, 1], [1, 0], [2, 0]], [[1, 1], [0, 1], [0, 2]], [[1, 1], [0, 0], [0, 0]], [[1, 2], [1, 1], [2, 0]], [[1, 2], [1, 0], [2, 1]], [[1, 2], [0, 1], [0, 0]], [[1, 2], [0, 0], [0, 1]], [[2, 0], [1, 1], [0, 1]], [[2, 0], [1, 0], [0, 2]], [[2, 0], [0, 1], [1, 1]], [[2, 0], [0, 0], [1, 2]], [[2, 1], [1, 1], [0, 2]], [[2, 1], [1, 0], [0, 0]], [[2, 1], [0, 1], [1, 2]], [[2, 1], [0, 0], [1, 0]], [[2, 2], [1, 1], [0, 0]], [[2, 2], [1, 0], [0, 1]], [[2, 2], [0, 1], [1, 0]], [[2, 2], [0, 0], [1, 1]]]
+const drawGraph = (result) => {
+    console.log(result)
+    result.forEach((triplet) => {
+        g.addEdge(triplet[0].toString(), triplet[2].toString(), { label: triplet[1].toString(), directed: true });
+    })
 
-let states = [[0, 0], [0, 1], [0, 2], [1, 0], [1, 1], [1, 2], [2, 0], [2, 1], [2, 2]]
+    var layouter = new Graph.Layout.Spring(g);
+    layouter.layout();
 
-// states.forEach(state => {
-//     g.addNode(state.toString())
-// });
+    var renderer = new Graph.Renderer.Raphael('canvas', g, 1200, 600);
+    renderer.draw();
 
-result.forEach((triplet) => {
-    g.addEdge(triplet[0].toString(), triplet[2].toString(), { label: triplet[1].toString(), directed: true });
-})
+}
 
-
-// g.edges.forEach((edge) => {
-//     edge.directed = true;
-//     edge.style = { label: 'hello' };
-// })
-// console.log(g.edges)
-
-var layouter = new Graph.Layout.Spring(g);
-layouter.layout();
-
-var renderer = new Graph.Renderer.Raphael('canvas', g, 1200, 900);
-renderer.draw();
+const eColi = document.getElementById('e-coli');
+eColi.addEventListener('click', (e) => onclick(e));
